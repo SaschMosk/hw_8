fa4ce88f7c550375bdf0c3f5ded4c1e961ea9316
LAPTOP-ETPA4QUH\sasch <saschamosk@gmail.com>
2018-03-07 23:55:09 +0300

    add files

--- homework_13.py
+++ homework_13.py
@@ -1,21 +1,10 @@
 import subprocess
 import os
 
-if not os.path.exists('C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Converted_fotos'):
-    os.mkdir(r'C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Converted_fotos')
+if not os.path.exists('C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Result'):
+    os.mkdir(r'C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Result')
 
 # for file in 'C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Source':
 process = subprocess.call('C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/convert.exe '
                           'C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Source/trump_mug.jpg '
-                          '-resize 200 C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Converted_fotos')
-
-
-
-
-# args = ['convert', fname, '-compress', 'none',
-#             os.path.splitext(os.path.basename(fname))[0]+'.png']
-#     prog_status = subprocess.call(args)
-#     return "{0} processed. Exited with status code {1}".format(
-#         fname, prog_status)
-
-# convert input.jpg -resize 200 output.jpg
+                          '-resize 200 C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Result/trump_mug.jpg')

3a51d62bb0213da1c53ad63680e007a000301077
LAPTOP-ETPA4QUH\sasch <saschamosk@gmail.com>
2018-03-29 01:22:01 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html

--- homework_18.py
+++ homework_18.py
@@ -1,27 +1,26 @@
 import osa
-import os
 
 #task 1
 
-# client = osa.Client('http://www.webservicex.net/ConvertTemperature.asmx?WSDL')
-#
-# def convert_to_C(data):
-#     return round(client.service.ConvertTemp(data, FromUnit='degreeFahrenheit', ToUnit='degreeCelsius'), 2)
-#
-# tempo = []
-# with open('temps.txt', 'r') as temps:
-#     for line in temps:
-#         tempo.append(int(line[0:1]))
-# print(tempo)
-#
-#
-# def mean_C(tempo):
-#     tempo_C = []
-#     for temp in tempo:
-#         tempo_C.append(convert_to_C(temp))
-#     return sum(tempo_C)/len(tempo_C)
-#
-# mean_C(tempo)
+client = osa.Client('http://www.webservicex.net/ConvertTemperature.asmx?WSDL')
+
+def convert_to_C(data):
+    return round(client.service.ConvertTemp(data, FromUnit='degreeFahrenheit', ToUnit='degreeCelsius'), 2)
+
+tempo = dict()
+with open('temps.txt') as f:
+    temps_values = []
+    for line in f:
+        line = line.split(' ')
+        temps_values.append(int(line[0]))
+
+def mean_C(tempo):
+    tempo_C = []
+    for temp in tempo:
+        tempo_C.append(convert_to_C(temp))
+    return sum(tempo_C)/len(tempo_C)
+
+print(mean_C(temps_values))
 
 #task 2
 
@@ -29,20 +28,46 @@ client = osa.Client("http://fx.currencysystem.com/webservices/CurrencyServer4.as
 
 help(client.service)
 
-expenses = []
-with open('travel.txt', 'r') as travel:
-    for line in travel:
-        expenses.append(line)
-print(expenses)
+expenses = dict()
+with open('currencies.txt') as f:
+    for line in f:
+        line = line.split(' ')
+        money = [line[1], line[2][0:3]]
+        expenses[line[0]] = money
 
-def convert_to_rubl(data):
-    return client.service.ConvertToNum(data, FromCurrency=expenses['Currency'], toCurrency='RUB', amount=1000,
-                                       rounding=True)
+def convert_to_rubl(currency, amount):
+    if currency in client.service.Currencies(licenseKey=''):
+        return round(client.service.ConvertToNum(licenseKey='', fromCurrency=currency, toCurrency='RUB', amount=amount,
+                                           rounding=True), 0)
 
-def calculate_expenses_rubl(expenses):
-    for expense in expenses:
-        convert_to_rubl(expense)
-        return sum(expenses)
+def calculate_expenses_rubl(expenses_list):
+    expenses_in_rubl = []
+    for expense in expenses_list.items():
+        expenses_in_rubl.append(convert_to_rubl(expense[1][1], expense[1][0]))
+    return sum(expenses_in_rubl)
+
+print(calculate_expenses_rubl(expenses))
 
 #task 3
 
+client = osa.Client('http://www.webservicex.net/ConvertLength.asmx?WSDL')
+
+distances = dict()
+with open('currencies.txt') as f:
+    for line in f:
+        line = line.split(' ')
+        distances[line[0]] = line[1]
+print(distances)
+
+def convert_to_km(distance):
+    return round(client.service.ChangeLengthUnit(distance, FromUnit='mi', ToUnit='km'), 2)
+
+help(client.service)
+
+def calculate_distance(distances):
+    distances_in_km = []
+    for distance in distances.items():
+        distances_in_km.append(convert_to_km(distance[1]))
+    return sum(distances_in_km)
+
+print(calculate_distance(distances))

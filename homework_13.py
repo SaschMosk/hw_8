671fdeaf703680b0533523bdcfdddb46f5f48d95
LAPTOP-ETPA4QUH\sasch <saschamosk@gmail.com>
2018-04-16 19:14:01 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html

--- homework_13.py
+++ homework_13.py
@@ -1,18 +1,22 @@
 import subprocess
 import os
 
+data_path = f'{os.getcwd()}\PY1_Lesson_2.5\homework\\'
+Result_folder_path = data_path + 'Result'
+Source_folder_path = data_path + 'Source'
+Converter_path = data_path + 'convert.exe'
 
 def make_folder_for_output():
-    if not os.path.exists('C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Result'):
-        os.mkdir(r'C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Result')
-
+    if not os.path.exists(Result_folder_path):
+        os.mkdir(Result_folder_path)
+make_folder_for_output()
 
 def resize_fotos():
-    fotos_list = os.listdir(path='C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Source')
+    fotos_list = os.listdir(path=Source_folder_path)
     for foto in fotos_list:
-        in_converter = str('C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Source/') + str(foto)
-        out_converter = str('C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/Result/') + str(foto)
-        arguments = ['C:/Users/sasch/Downloads/Python_course-master/PY1_Lesson_2.5/homework/convert.exe ',
+        in_converter = str(Source_folder_path + '\\') + str(foto)
+        out_converter = str(Result_folder_path + '\\') + str(foto)
+        arguments = [Converter_path,
                      in_converter, '-resize', '200', out_converter]
         subprocess.run(arguments)
 
